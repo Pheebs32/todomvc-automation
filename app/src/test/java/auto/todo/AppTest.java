d35ce45deebd2a711fdcd73dfd1942d3676cc461
Phoebe Swaine <peswaine@gmail.com>
2023-05-17 00:32:27 +0100

    Fourth test implemented - Toggle completed for todo item

--- app/src/test/java/auto/todo/AppTest.java
+++ app/src/test/java/auto/todo/AppTest.java
@@ -91,6 +91,48 @@ public class AppTest {
         assertTrue(todoList.stream().noneMatch(el -> "Buy groceries".equals(el.getText())));
     }
 
+    @Test
+    public void togglesTodoCompleted() {
+        // Create new items
+        driver.get("https://todomvc.com/examples/vanillajs/");
+        WebElement todoInput = driver.findElement(By.className("new-todo"));
+        todoInput.sendKeys("Buy groceries");
+        todoInput.sendKeys(Keys.ENTER);
+
+        todoInput.sendKeys("Buy dog food");
+        todoInput.sendKeys(Keys.ENTER);
+
+        // Get the item by name
+        WebElement todoLi = driver.findElements(By.cssSelector(".todo-list li"))
+                .stream()
+                .filter(el -> "Buy groceries".equals(el.getText()))
+                .findFirst()
+                .orElseThrow(() -> new AssertionError("Test data missing!"));
+
+        // Toggle item completed
+        WebElement todoToggleCheckbox = todoLi.findElement(By.cssSelector("input.toggle"));
+        todoToggleCheckbox.click();
+
+        // Verify count
+        WebElement todoCount = driver.findElement(By.cssSelector(".todo-count > strong"));
+        assertEquals(1, Integer.parseInt(todoCount.getText()));
+
+        // Show completed items
+        WebElement completedLink = driver.findElement(By.cssSelector("a[href='#/completed']"));
+        completedLink.click();
+
+        // Verify completed items count
+        List<WebElement> completedTodos = driver.findElements(By.cssSelector(".todo-list li"));
+        assertEquals(1, completedTodos.size());
+
+        // Show active items
+        WebElement activeLink = driver.findElement(By.cssSelector("a[href='#/active']"));
+        activeLink.click();
+
+        // Verify active items count
+        List<WebElement> activeTodos = driver.findElements(By.cssSelector(".todo-list li"));
+        assertEquals(1, activeTodos.size());
+    }
 
     @AfterEach
     void closeBrowser() {
