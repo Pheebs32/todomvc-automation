3843f6386ec75e4f879092d3ccce9d872c356a35
Phoebe Swaine <peswaine@gmail.com>
2023-05-16 23:11:36 +0100

    First test implemented - Adding a todo item

--- app/src/test/java/auto/todo/AppTest.java
+++ app/src/test/java/auto/todo/AppTest.java
@@ -6,8 +6,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 import javax.security.auth.Subject;
+import org.openqa.selenium.Keys;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     private static FirefoxDriver driver;
@@ -15,6 +20,24 @@ public class AppTest {
     @BeforeAll
     static void launchBrowser() {
         driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void testAddTodoItem() {
+        // Add a new item
+        driver.get("https://todomvc.com/examples/vanillajs/");
+        String todoText = "Buy groceries";
+        driver.findElement(By.cssSelector(".new-todo")).sendKeys(todoText);
+        WebElement search = driver.findElement(By.cssSelector(".new-todo"));
+        search.sendKeys(Keys.RETURN);
+
+        // Wait for the item to be added
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(".todo-list li:nth-child(1) label"), todoText));
+
+        // Verify that the item is added correctly
+        String addedTodoText = driver.findElement(By.cssSelector(".todo-list li:nth-child(1) label")).getText();
+        assertEquals(todoText, addedTodoText);
     }
 
     @AfterAll
